2925b2c5082f3c71d9daeed13d7f33e55425bcf3
0x4D61696B <365945ba@gmail.com>
2016-01-31 09:43:50 +0100

    Updated for 1.6
    - Added check for repair tutorial so the addon won't cause the player to become stuck

--- bAutomaticRepairs.lua
+++ bAutomaticRepairs.lua
@@ -4,8 +4,8 @@
 -- =============================================================================
 
 require "math"
-require "string"
 require "table"
+require "unicode"
 require "lib/lib_ChatLib"
 require "lib/lib_Debug"
 
@@ -20,6 +20,8 @@ local c_MaxDurability = 1000
 local itemIDs = {}
 local repairCost = 0
 
+local g_Enabled = true
+
 
 -- =============================================================================
 --  Functions
@@ -31,7 +33,7 @@ end
 
 function GetRepairCost(itemInfo)
     local durabilityPercent = 1 - (itemInfo.durability.current / c_MaxDurability)
-    
+
     return Game.GetItemRepairCost(itemInfo.itemTypeId, durabilityPercent)
 end
 
@@ -47,14 +49,16 @@ function OnRepairResponse(args)
 end
 
 function OnTerminalAuthorized(args)
-    if (args.terminal_type == "GARAGE") then
+    Debug.Table("OnTerminalAuthorized", {addonEnabled = g_Enabled, terminalInfo = args})
+
+    if (g_Enabled and args.terminal_type == "GARAGE") then
         local itemList = Player.GetItemIdList("gear")
         itemIDs = {}
         repairCost = 0
-        
+
         for _, itemGuid in pairs(itemList) do
             local itemInfo = Player.GetItemInfo(itemGuid)
-            
+
             if (itemInfo and itemInfo.durability) then
                 if (itemInfo.durability.current < c_MaxDurability) then
                     repairCost = repairCost + GetRepairCost(itemInfo)
@@ -62,9 +66,28 @@ function OnTerminalAuthorized(args)
                 end
             end
         end
-        
+
         if (#itemIDs > 0) then
             Player.RequestRepairItems(itemIDs)
         end
+    end
+end
+
+function OnTrackerUpdate(args)
+    if not (args.json) then return end
+
+    local args = jsontotable(args.json)
+
+    if (args.id and args.objectives and args.id == "mission_867") then
+        Debug.Table("OnTrackerUpdate", args)
+
+        for _, objective in pairs(args.objectives) do
+            if not (objective.completed) then
+                g_Enabled = false
+                return
+            end
+        end
+
+        g_Enabled = true
     end
 end
